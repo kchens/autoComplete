d966b772ab1fcec1c0b435c1716c7d49894b6661
kchens <kkch3n@gmail.com>
2015-03-12 01:08:55 -0700

    adds completed autoComplete

--- autocomplete.js
+++ autocomplete.js
@@ -24,19 +24,54 @@ window.onload = function() {
     "Scheme"
   ];
 
-  // add an eventlistener to the #search
-  //    on every keyup, fire function updateSearchResults
-  // updateSearchResult
-  //    var results = [];
-  //    (1) gets the letters submitted into the text field
-  //    (2) check each word in the dictionary for the letters
-  //    (3) store the matching words into a results array
-  //    (4) find the #search-results div
-  //      (5) loop through results array and create a new div
-
   var searchBar = document.getElementById('search');
-  searchBar.addEventListener('keyup', function(e) {
-    searchBar.childNodes[0] = [];
 
-  });
+  var getSearchTerm = function() {
+    var searchTerm = event.target.value;
+    console.log(searchTerm);
+    return searchTerm;
+  };
+
+  var updateSearchResults = function(searchTerm) {
+    var regexTerm;
+    var results = [];
+
+    regexTerm = new RegExp('^' + searchTerm, 'i');
+    dictionary.forEach(function(word) {
+      if (regexTerm.test(word) && !(searchTerm == '')) {
+        results.push(word);
+      };
+    });
+    return results;
+  };
+
+  var removeChildNodes = function(divSearchResults){
+    while (divSearchResults.hasChildNodes()) {
+      divSearchResults.removeChild(divSearchResults.lastChild);
+    }
+  }
+
+  var createMatchedWord = function(matchedWord, divSearchResults) {
+    var matchedWordInput = document.createElement('input');
+
+    matchedWordInput.setAttribute('disabled', 'disabled');
+    matchedWordInput.setAttribute('class', 'matched-word');
+    matchedWordInput.setAttribute('value', matchedWord);
+
+    divSearchResults.appendChild(matchedWordInput);
+  }
+
+  var addUpdatedSearchResults = function() {
+    var divSearchResults = document.getElementById('search-results');
+    removeChildNodes(divSearchResults);
+
+    var searchTerm = getSearchTerm();
+    var searchResults = updateSearchResults(searchTerm);
+
+    searchResults.forEach(function(result, index, array) {
+      createMatchedWord(result, divSearchResults);
+    });
+  }
+
+  searchBar.addEventListener('keyup', addUpdatedSearchResults, false);
 };
